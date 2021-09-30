fedf4c8f32e41e40c81e0efb15173d94fbeed48e
Nicolay Egorov <nickphoenix21@icloud.com>
2021-09-30 22:49:37 +0700

    Правка функций

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 const getRandomNumber = (min, max) => {
-  if (min < 0 || max < 0) {
-    return 'Значение не совпадает с диапазоном';
-  }
-  if (min >= max) {
-    return 'Минимальное значение не может ровняться или быть больше максимального';
+  if (min < 0 || max < 0 || max < min) {
+    return null;
   }
   return Math.round(Math.random() * (max - min + 1)) + min;
 };
 
 const getRandomFractionalNumber = (min, max, point) => {
-  if (min < 0 || max < 0) {
-    return 'Значение не совпадает с диапазоном';
-  }
-  if (min >= max) {
-    return 'Минимальное значение не может ровняться или быть больше максимального';
+  if (min < 0 || max < 0 || max < min) {
+    return null;
   }
   return (Math.random() * (max - min + 1) + min).toFixed(point);
 };
 
 getRandomNumber(1, 9);
 
-getRandomFractionalNumber(1, 9, 4);
+getRandomFractionalNumber(0, 9, 5);
