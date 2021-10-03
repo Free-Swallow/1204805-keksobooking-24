2b318414adbeb1549b420adfb203645516381319
Nicolay Egorov <nickphoenix21@icloud.com>
2021-10-04 02:56:58 +0700

    Обновление ф-ий

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const validationValues = (min, max) => {
-     return min < 0 || max < 0 || max < min ? false : true;
-}
+  return (min < 0 || max < 0 || max < min) ? false : true;
+};
 
 const getRandomNumber = (min, max) => {
-  return validationValues(min,max) ? Math.round(Math.random() * (max - min + 1)) + min : null;
+  return validationValues(min, max) ? Math.round(Math.random() * (max - min + 1)) + min : null;
 };
 
 const getRandomFractionalNumber = (min, max, point) => {
-  return validationValues(min,max) ? Math.round(Math.random() * Math.random() * (max - min + 1) + min).toFixed(point) : null;
-  }
+  return validationValues(min, max) ? Math.round(Math.random() * Math.random() * (max - min + 1) + min).toFixed(point) : null;
+};
 
 getRandomNumber(1, 9);
 getRandomFractionalNumber(1, 9, 4);
