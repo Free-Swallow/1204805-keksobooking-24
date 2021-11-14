30ba875e62c354db8da73a57c26d9424354744c2
Nicolay Egorov <nickphoenix21@icloud.com>
2021-11-15 06:00:39 +0700

    Add map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,11 @@
-// const mapWrapper = document.querySelector('#map-canvas');
-// const LAT_CENTER_MAP = 35.66000;
-// const LNG_CENTER_MAP = 139.75000;
-// const SCALE = 13;
+import {ROUNDING_COORDINATE, createFlyers} from './generating-offers.js';
+import {createCard} from './popup.js';
+
+const mapWrapper = document.querySelector('#map-canvas');
+const address = document.querySelector('#address');
+const LAT_CENTER_MAP = 35.65000;
+const LNG_CENTER_MAP = 139.75000;
+const SCALE = 13;
 
 const disabledMap = () => {
   const mapFilter = document.querySelector('.map__filters');
@@ -11,7 +15,7 @@ const disabledMap = () => {
   // featuresMap.setAttribute('disabled', 'disabled');
 };
 
-// disabledMap();
+disabledMap();
 
 const activateMap = () => {
   const mapFilter = document.querySelector('.map__filters');
@@ -19,25 +23,89 @@ const activateMap = () => {
   mapFilter.classList.remove('ad-form--disabled');
 };
 
-// const map = L.map(mapWrapper)
-//   .on('load', activedMap)
-//   .setView({
-//     lat: LAT_CENTER_MAP,
-//     lng: LNG_CENTER_MAP,
-//   }, SCALE);
+const map = L.map(mapWrapper)
+  .on('load', activateMap)
+  .setView({
+    lat: LAT_CENTER_MAP,
+    lng: LNG_CENTER_MAP,
+  }, SCALE);
 
-// L.tileLayer(
-//   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-//   {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//   },
-// ).addTo(map);
+const markerGroup = L.layerGroup().addTo(map);
 
-// const marker = L.marker({
-//   lat: 35.66660,
-//   lng: 139.75000,
-// },);
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
 
-// marker.aadTo(map);
+const mainPin = L.icon(
+  {
+    iconUrl: '../img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [52, 26],
+  },
+);
+
+const offerPin = L.icon(
+  {
+    iconUrl: '../img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [40, 20],
+  },
+);
+
+const marker = L.marker(
+  {
+    lat: LAT_CENTER_MAP,
+    lng: LNG_CENTER_MAP,
+  },
+  {
+    draggable: true,
+    icon: mainPin,
+  },
+);
+
+marker.addTo(map);
+
+const onAddressChange = (evt) => {
+  const {lat, lng} = evt.target.getLatLng();
+  address.value = `${lat.toFixed(ROUNDING_COORDINATE)} ${lng.toFixed(ROUNDING_COORDINATE)}`;
+};
+
+marker.addEventListener('moveend', onAddressChange);
+
+// const onResetMainPin = () => {
+//   marker.setLatLng({
+//       lat: LAT_CENTER_MAP,
+//       lng: LNG_CENTER_MAP,
+//   });
+
+// const onResetMap = () => {
+//  map.setLatLng({
+//    lat: LAT_CENTER_MAP,
+//    lng: LNG_CENTER_MAP,
+//  }, SCALE);
+// };
+
+const assemblingOffers = (offers) => {
+  offers.forEach((offer) => {
+    const {location} = offer;
+
+    const testMarker = L.marker({
+      lat: location.lat,
+      lng: location.lng,
+    },
+    {
+      icon: offerPin,
+    });
+    testMarker
+      .addTo(markerGroup)
+      .bindPopup(() => createCard(offer),
+      );
+  });
+};
+
+assemblingOffers(createFlyers());
 
 export {disabledMap, activateMap};
